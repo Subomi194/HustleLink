0ede6b018415072c5fdc0842ef3e5418683a389e
Subomi194 <subomiariyo@gmail.com>
2026-06-27 15:44:57 +0100

    deployment2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -164,6 +164,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'HustleLink API',
